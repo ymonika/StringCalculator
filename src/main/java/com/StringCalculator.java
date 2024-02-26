08f57e3bc6ef329119c7bf0071bcf647136e6a3f
ymonika <ymonika123@gmail.com>
2024-02-26 14:19:03 +0530

    [MY] add() method changes and test case

--- src/main/java/com/StringCalculator.java
+++ src/main/java/com/StringCalculator.java
@@ -1,8 +1,14 @@
 package com;
 
+import java.util.Arrays;
+
 public class StringCalculator {
 
     public int add(String str) {
-        return 0;
+        return Arrays.stream(str.split(","))
+                .filter(s -> !s.isEmpty()).map(Integer::parseInt).reduce(Integer::sum)
+                .orElse(0);
+
     }
+
 }
--- src/test/java/com/StringCalculatorTest.java
+++ src/test/java/com/StringCalculatorTest.java
@@ -12,4 +12,12 @@ public class StringCalculatorTest {
         StringCalculator stringCalculator = new StringCalculator();
         assertEquals(0, stringCalculator.add(""));
     }
+
+    @Test
+    public void testTwoAndThreeNumberString() {
+        StringCalculator calculator = new StringCalculator();
+        assertEquals(1, calculator.add("0,1"));
+        assertEquals(6, calculator.add("1,5"));
+        assertEquals(0, calculator.add(""));
+    }
 }
